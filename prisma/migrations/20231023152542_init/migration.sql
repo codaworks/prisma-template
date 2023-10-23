c4a3b019fb09e211f8c8f572ba912f991b1df1ca
Mordechai Meisels <mordechai@codaworks.com>
2023-10-23 11:28:26 -0400

    Create template for future projects

--- prisma/migrations/20231023152542_init/migration.sql
+++ prisma/migrations/20231023152542_init/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "user" (
+CREATE TABLE "account_user" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "account_user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+CREATE UNIQUE INDEX "account_user_email_key" ON "account_user"("email");
